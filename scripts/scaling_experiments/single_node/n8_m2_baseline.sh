40116501d35ebe22c6d5a0ad1237ac837e7c2cc9
Marie Weiel <marie.weiel@kit.edu>
2024-07-23 16:07:59 +0200

    reintroduce BASE_DIR and clean up bash scripts + comments

--- scripts/scaling_experiments/single_node/n8_m2_baseline.sh
+++ scripts/scaling_experiments/single_node/n8_m2_baseline.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
-#SBATCH --job-name=n8_m2_base         # job name
-#SBATCH --partition=cpuonly           # queue for resource allocation
-#SBATCH --mem=501600mb
-#SBATCH --time=3-00:00:00             # wall-clock time limit
-#SBATCH --cpus-per-task=76            # number of CPUs required per (MPI) task
-#SBATCH --mail-type=ALL               # Notify user by email when certain event types occur.
-#SBATCH --account=hk-project-test-aihero2
-
+#SBATCH --job-name=n8_m2_base  # Job name
+#SBATCH --partition=cpuonly    # Queue for resource allocation
+#SBATCH --mem=501600mb         # Memory required per node
+#SBATCH --time=3-00:00:00      # Wall-clock time limit
+#SBATCH --cpus-per-task=76     # Number of CPUs required per (MPI) task
+#SBATCH --mail-type=ALL        # Notify user by email when certain event types occur.
 
 # Overwrite base directory by running export BASE_DIR="/some/alternative/path/here" before submitting the job.
 BASE_DIR=${BASE_DIR:-/hkfs/work/workspace/scratch/ku4408-special-couscous/}
 
-export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
-export PYDIR=${BASE_DIR}/special-couscous/specialcouscous
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}  # Set number of threads to number of CPUs per task as provided by SLURM.
+export PYDIR=${BASE_DIR}/special-couscous/specialcouscous  # Set path to Python package directory.
 
-ml purge                          # Unload all currently loaded modules.
-ml load compiler/gnu              # Load required modules.
+ml purge              # Unload all currently loaded modules.
+ml load compiler/gnu  # Load required modules.
 ml load mpi/openmpi
-source "${BASE_DIR}"/special-couscous-venv/bin/activate  # Activate venv.
 
+source "${BASE_DIR}"/special-couscous-venv/bin/activate  # Activate virtual environment.
+
+# Set hyperparameters of synthetic dataset and random forest model.
+# 100 trees were not trainable in serial in 3 days. Choose a number evenly divisible by 64 (scaling experiments).
+# Training one tree takes approx. 1 h.
 N_SAMPLES=100000000
 N_FEATURES=100
-N_TREES=64  # 100 trees were not trainable in serial in 3 days. Choose a number evenly divisible by 64 (scaling experiments). Training one tree takes approx. 1 h.
+N_TREES=64
 
-SCRIPT="rf_serial_synthetic.py"
+SCRIPT="rf_serial_synthetic.py"  # Set script name.
 
-
+# Create directory to save results to.
 RESDIR=${BASE_DIR}/results/single_node_experiments/n8_m2_baseline_${SLURM_JOB_ID}/
 mkdir "${RESDIR}"
 cd "${RESDIR}" || exit
 
-python -u ${PYDIR}/${SCRIPT} --n_samples ${N_SAMPLES} --n_features ${N_FEATURES} --n_trees ${N_TREES}
+# Run script.
+python -u "${PYDIR}"/${SCRIPT} --n_samples ${N_SAMPLES} --n_features ${N_FEATURES} --n_trees ${N_TREES}
