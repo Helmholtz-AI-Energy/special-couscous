3d7983d9131a30ec119e4d0690f0ce6a73c3503f
Marie Weiel <marie.weiel@kit.edu>
2024-08-07 10:11:51 +0200

    make executable, add shebang, docstring, and comments

--- scripts/examples/process_slurm_out.py
+++ scripts/examples/process_slurm_out.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+"""Exemplary script for processing slurm output files."""
+
+import pathlib
+
 import pandas as pd
 
 from specialcouscous.utils import dataframe_from_slurm_output
@@ -9,8 +14,16 @@ from specialcouscous.utils.plot import (
 pd.set_option("display.max_columns", 20)
 pd.set_option("display.max_rows", None)
 
-exp_path = "../exps/single_node/slurm"
+exp_path = pathlib.Path(
+    "../exps/single_node/slurm"
+)  # Set path to SLURM job output files.
 
-df = dataframe_from_slurm_output(exp_path)
-plot_single_node_capacity(df, exp_path)
-plot_times_and_accuracy(df, exp_path)
+df = dataframe_from_slurm_output(
+    exp_path
+)  # Create a dataframe from SLURM output files.
+plot_single_node_capacity(
+    df, exp_path
+)  # Plot results from single-node capacity experiments.
+plot_times_and_accuracy(
+    df, exp_path
+)  # Plot training time and accuracy from single-node capacity experiments.
