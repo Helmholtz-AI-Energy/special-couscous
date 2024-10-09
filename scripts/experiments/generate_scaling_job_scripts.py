29227bb0f50d8ef803e4e83ec3020af00559eb02
Marie Weiel <marie.weiel@kit.edu>
2024-10-09 12:09:07 +0200

    update general scaling job script generation script

--- scripts/experiments/generate_scaling_job_scripts.py
+++ scripts/experiments/generate_scaling_job_scripts.py
@@ -9,6 +9,9 @@ nodes = [2, 4, 8, 16, 32, 64]  # Number of nodes for scaling exps
 def generate_scaling_job_scripts(
     log_n_samples: int,
     log_n_features: int,
+    n_classes: int,
+    random_state_data: int,
+    random_state_model: int,
     n_trees: int,
     scaling_type: str,
     output_path: pathlib.Path,
@@ -39,6 +42,12 @@ def generate_scaling_job_scripts(
         The common logarithm of the number of samples to use.
     log_n_features : int
         The common logarithm of the number of features to use.
+    n_classes : int
+        The number of classes in the synthetic dataset.
+    random_state_data : int
+        The random state used for synthetic dataset generation and splitting.
+    random_state_model : int
+        The (base) random state used for initializing the (distributed) model.
     n_trees : int
         The number of trees to use. For weak scaling, this number serves as a baseline and is scaled up with the number
         of nodes.
@@ -52,14 +61,14 @@ def generate_scaling_job_scripts(
     for n_nodes in nodes:
         # ---- Weak scaling ----
         if scaling_type == "weak":
-            n_trees = (
+            n_trees_overall = (
                 n_trees * n_nodes
             )  # Number of trees is scaled with number of nodes.
             time = 3600  # All experiments should take approx. the same time (in min).
             mem = 243200  # Use standard nodes.
         # ---- Strong scaling ----
         elif scaling_type == "strong":
-            n_trees = n_trees  # Number of trees stays the same.
+            n_trees_overall = n_trees  # Number of trees stays the same.
             time = int(
                 limit / n_nodes * 1.2
             )  # Run time should decrease with increasing number of nodes.
@@ -88,34 +97,33 @@ def generate_scaling_job_scripts(
 #SBATCH --ntasks-per-node=1           # One MPI rank per node
 
 # Overwrite base directory by running export BASE_DIR="/some/alternative/path/here" before submitting the job.
-BASE_DIR=${{BASE_DIR:-/hkfs/work/workspace/scratch/ku4408-special-couscous/}}
+BASE_DIR=${{BASE_DIR:-/hkfs/work/workspace/scratch/ku4408-SpecialCouscous}}
 
 export OMP_NUM_THREADS=${{SLURM_CPUS_PER_TASK}}
-export PYDIR=${{BASE_DIR}}/special-couscous/specialcouscous
 
 ml purge              # Unload all currently loaded modules.
 ml load compiler/gnu  # Load required modules.
-ml load mpi/openmpi
-source "${{BASE_DIR}}"/special-couscous-venv/bin/activate  # Activate venv.
+ml load mpi/openmpi/4.1
+source "${{BASE_DIR}}"/special-couscous-venv-openmpi4/bin/activate  # Activate venv.
 
-N_SAMPLES={10**log_n_samples}
-N_FEATURES={10**log_n_features}
-N_TREES={n_trees}
+SCRIPT="special-couscous/scripts/examples/rf_parallel_synthetic.py"
 
-SCRIPT="scripts/examples/rf_parallel_synthetic.py"
-
-RESDIR="${{BASE_DIR}}"/results/{scaling_type}_scaling/n6_m4_nodes_${{SLURM_NPROCS}}_${{SLURM_JOB_ID}}/
-mkdir "${{RESDIR}}"
+RESDIR="${{BASE_DIR}}"/results/{scaling_type}_scaling/n{log_n_samples}_m{log_n_features}/nodes_${{SLURM_NPROCS}}/${{SLURM_JOB_ID}}_{random_state_data}_{random_state_model}/
+mkdir -p "${{RESDIR}}"
 cd "${{RESDIR}}" || exit
 
-srun python -u ${{PYDIR}}/${{SCRIPT}} \\
-    --n_samples ${{N_SAMPLES}} \\
-    --n_features ${{N_FEATURES}} \\
-    --n_trees ${{N_TREES}} \\
+srun python -u ${{BASE_DIR}}/${{SCRIPT}} \\
+    --n_samples {10**log_n_samples} \\
+    --n_features {10**log_n_features} \\
+    --n_trees {n_trees_overall} \\
+    --n_classes {n_classes} \\
+    --random_state {random_state_data} \\
+    --random_state_model {random_state_model} \\
+    --detailed_evaluation \\
+    --save_model \\
     --output_dir ${{RESDIR}} \\
     --output_label ${{SLURM_JOB_ID}} \\
-    --detailed_evaluation \\
-    --save_model
+    --log_path ${{RESDIR}}
                                 """
 
         with open(output_path / job_script_name, "wt") as f:
@@ -144,6 +152,17 @@ if __name__ == "__main__":
         help="The common logarithm of the number of features to use.",
     )
     parser.add_argument(
+        "--random_state_data",
+        type=int,
+        help="The random state used for synthetic data generation.",
+    )
+    parser.add_argument(
+        "--random_state_model",
+        type=int,
+        default=None,
+        help="Optional random seed used to initialize the random forest classifier",
+    )
+    parser.add_argument(
         "--n_trees",
         type=int,
         help="The number of trees to use. Overall number for strong scaling and baseline number for weak scaling.",
@@ -164,10 +183,13 @@ if __name__ == "__main__":
     args = parser.parse_args()
     # Generate job scripts and possibly submit them to the cluster.
     generate_scaling_job_scripts(
-        args.log_n_samples,
-        args.log_n_features,
-        args.n_trees,
-        args.scaling_type,
-        args.output_path,
-        args.submit,
+        log_n_samples=args.log_n_samples,
+        log_n_features=args.log_n_features,
+        n_classes=args.n_classes,
+        random_state_data=args.random_state_data,
+        random_state_model=args.random_state_model,
+        n_trees=args.n_trees,
+        scaling_type=args.scaling_type,
+        output_path=args.output_path,
+        submit=args.submit,
     )
