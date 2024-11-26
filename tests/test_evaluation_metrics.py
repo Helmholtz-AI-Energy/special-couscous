7f759bab94af8e030ff8dabd03ba32c0220498c8
Katharina Fluegel <katharina.fluegel@kit.edu>
2024-11-26 10:42:43 +0100

    Add tests for precision, recall, and fbeta

--- tests/test_evaluation_metrics.py
+++ tests/test_evaluation_metrics.py
@@ -5,6 +5,28 @@ from specialcouscous import evaluation_metrics
 
 
 class TestEvaluationMetrics:
+    @staticmethod
+    def first_and_fill_rest(first: float, fill: float, total_length: int) -> np.ndarray[float]:
+        """
+        Create a numpy array (1D) of length total_length where the first value is first and all remaining values are
+        filled with the specified fill value.
+
+        Parameters
+        ----------
+        first : float
+            The value to set the first array element to.
+        fill : float
+            The value to set all other array elements to.
+        total_length : int
+            The total length of the array (including the first value).
+
+        Returns
+        -------
+        np.ndarray[float]
+            The created array [first, fill, ... fill] of length total_length.
+        """
+        return np.concat([np.array([first]), np.full(total_length - 1, fill)])
+
     def test_accuracy_score(self, n_classes=10):
         # balanced case
         y_true = np.arange(n_classes).repeat(5)
@@ -43,3 +65,82 @@ class TestEvaluationMetrics:
             confusion_matrix = sklearn.metrics.confusion_matrix(y_true, y_pred)
             assert evaluation_metrics.balanced_accuracy_score(confusion_matrix) == expected_accuracy
 
+
+    def test_precision_score(self, n_classes=10):
+        # balanced case
+        y_true = np.arange(n_classes).repeat(5)
+        labels_prediction_and_expected_accuracy = [
+            (y_true, y_true, np.ones(n_classes)),  # all correct
+            (y_true, (y_true + 1) % n_classes, np.zeros(n_classes)),  # all false
+            # all zero: for first class: 5 correct predictions, but also 5 * (n_classes - 1) incorrect predictions
+            # -> 1 / n_classes, no prediction at all for all other classes -> nan
+            (y_true, np.zeros_like(y_true), self.first_and_fill_rest(1 / n_classes, np.nan, n_classes)),
+        ]
+        # imbalanced case: each class 5 times, except for first class: 5 * (1 + n_classes) times
+        y_true = np.concat([np.arange(n_classes), np.zeros(n_classes)]).repeat(5)
+        n_samples = 2 * n_classes
+        labels_prediction_and_expected_accuracy += [
+            (y_true, y_true, np.ones(n_classes)),  # all correct
+            (y_true, (y_true + 1) % n_classes, np.zeros(n_classes)),  # all false
+            # all zero: for first class: 5 * (1 + n_classes) correct predictions, but also n_classes - 1 incorrect
+            # predictions -> (1 + n_classes) / (2 * n_classes), no prediction at all for all other classes -> nan
+            (y_true, np.zeros_like(y_true), self.first_and_fill_rest((1 + n_classes) / n_samples, np.nan, n_classes)),
+        ]
+        for y_true, y_pred, expected_precision in labels_prediction_and_expected_accuracy:
+            confusion_matrix = sklearn.metrics.confusion_matrix(y_true, y_pred)
+            actual_precision = evaluation_metrics.precision_score(confusion_matrix)
+            np.testing.assert_array_equal(actual_precision, expected_precision, strict=True)
+
+    def test_recall_score(self, n_classes=10):
+        # balanced case
+        y_true = np.arange(n_classes).repeat(5)
+        labels_prediction_and_expected_accuracy = [
+            (y_true, y_true, np.ones(n_classes)),  # all correct
+            (y_true, (y_true + 1) % n_classes, np.zeros(n_classes)),  # all false
+            # all zero: only first class correct
+            (y_true, np.zeros_like(y_true), self.first_and_fill_rest(1., 0., n_classes)),
+        ]
+        # imbalanced case: each class 5 times, except for first class: 5 * (1 + n_classes) times
+        y_true = np.concat([np.arange(n_classes), np.zeros(n_classes)]).repeat(5)
+        labels_prediction_and_expected_accuracy += [
+            (y_true, y_true, np.ones(n_classes)),  # all correct
+            (y_true, (y_true + 1) % n_classes, np.zeros(n_classes)),  # all false
+            # all zero: only first class correct
+            (y_true, np.zeros_like(y_true), self.first_and_fill_rest(1., 0., n_classes)),
+        ]
+        for y_true, y_pred, expected_recall in labels_prediction_and_expected_accuracy:
+            confusion_matrix = sklearn.metrics.confusion_matrix(y_true, y_pred)
+            actual_recall = evaluation_metrics.recall_score(confusion_matrix)
+            np.testing.assert_array_equal(actual_recall, expected_recall, strict=True)
+
+    def test_fbeta_score(self, n_classes=10, beta=2):
+        # balanced case
+        y_true = np.arange(n_classes).repeat(5)
+        labels_prediction_and_expected_accuracy = [
+            (y_true, y_true, np.ones(n_classes)),  # all correct
+            (y_true, (y_true + 1) % n_classes, np.zeros(n_classes)),  # all false
+        ]
+        # all zero: only first class correct
+        precision = 1 / n_classes
+        recall = 1
+        f_beta = (1 + beta**2) * (precision * recall) / (beta**2 * precision + recall)
+        expected = self.first_and_fill_rest(f_beta, np.nan, n_classes)
+        labels_prediction_and_expected_accuracy += [(y_true, np.zeros_like(y_true), expected)]
+
+        # imbalanced case: each class 5 times, except for first class: 5 * (1 + n_classes) times
+        y_true = np.concat([np.arange(n_classes), np.zeros(n_classes)]).repeat(5)
+        labels_prediction_and_expected_accuracy += [
+            (y_true, y_true, np.ones(n_classes)),  # all correct
+            (y_true, (y_true + 1) % n_classes, np.zeros(n_classes)),  # all false
+        ]
+        # all zero: only first class correct
+        precision = (1 + n_classes) / (2 * n_classes)
+        recall = 1
+        f_beta = (1 + beta**2) * (precision * recall) / (beta**2 * precision + recall)
+        expected = self.first_and_fill_rest(f_beta, np.nan, n_classes)
+        labels_prediction_and_expected_accuracy += [(y_true, np.zeros_like(y_true), expected)]
+
+        for y_true, y_pred, expected_fbeta in labels_prediction_and_expected_accuracy:
+            confusion_matrix = sklearn.metrics.confusion_matrix(y_true, y_pred)
+            actual_fbeta = evaluation_metrics.fbeta_score(confusion_matrix, beta=beta)
+            np.testing.assert_array_equal(actual_fbeta, expected_fbeta, strict=True)
