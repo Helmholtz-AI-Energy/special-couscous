fe22dfa1b10da4e55c9cc155d1106e4549a0d78a
Katharina Fluegel <katharina.fluegel@kit.edu>
2025-01-20 15:55:19 +0100

    Add unit tests for scaling dataset

---
+++ tests/test_scaling_dataset.py
@@ -0,0 +1,150 @@
+import logging
+import pathlib
+
+import pytest
+
+from specialcouscous.synthetic_classification_data import (
+    generate_scaling_dataset,
+    write_scaling_dataset_to_hdf5,
+    read_scaling_dataset_from_hdf5,
+)
+from specialcouscous.utils import set_logger_config
+
+log = logging.getLogger("specialcouscous")  # Get logger instance.
+
+set_logger_config(
+    level=logging.INFO,  # Logging level
+    log_file=None,  # Logging path
+    log_to_stdout=True,  # Print log on stdout.
+    log_rank=False,  # Do not prepend MPI rank to logging messages.
+    colors=True,  # Use colors.
+)
+
+
+def test_create_scaling_dataset():
+    # use mostly default parameters from utils.parse_arguments()
+    n_classes = 4  # use only 4 classes
+    n_features = 20  # use only 20 features
+    frac_informative = 0.1
+    frac_redundant = 0.1
+    make_classification_kwargs = {
+        "n_clusters_per_class": 1,
+        "n_informative": int(frac_informative * n_features),
+        "n_redundant": int(frac_redundant * n_features),
+        "flip_y": 0,  # don't flip labels to check number of samples per slice more easily
+    }
+
+    # create scaling dataset for 10 ranks
+    global_train_set, training_slices, global_test_set = generate_scaling_dataset(
+        n_samples=1250,
+        n_features=n_features,
+        n_classes=n_classes,
+        n_ranks=10,
+        random_state=0,
+        test_size=0.2,
+        make_classification_kwargs=make_classification_kwargs,
+        stratified_train_test=True,
+    )
+
+    # check dataset size
+    # global train set = 1250 * 0.8 = 1000 samples
+    assert global_train_set.n_samples == 1000
+    # global test set = 1250 - 1000 = 250 samples
+    assert global_test_set.n_samples == 250
+    for local_train_set in training_slices.values():
+        # local train sets = 1000 / 10 (n_ranks) = 100 samples
+        assert local_train_set.n_samples == 100
+
+    # check dataset distribution: class- and feature-wise mean should not differ too much between the datasets
+    # e.g. mean of the first feature for class 0 should remain fairly consistent across all dataset slices
+    def subset_for_class(dataset, class_index):
+        return dataset.x[dataset.y == class_index]
+
+    for class_index in range(n_classes):
+        global_train_subset = subset_for_class(global_train_set, class_index)
+        global_train_mean = global_train_subset.mean(axis=0)
+        global_train_std = global_train_subset.std(axis=0)
+
+        global_test_mean = subset_for_class(global_test_set, class_index).mean(axis=0)
+
+        print(f"\nClass {class_index}\nGlobal train mean: {global_train_mean[:5]}")
+        print(f"Global test mean:  {global_test_mean[:5]}")
+        print(f"Global train std:  {global_train_std[:5]}")
+
+        # Check mean: mean of all smaller subsets should be within std of mean from global train set
+        # TODO: this check does not seem super informative since all means seem to be close to 0 with stds close to 1
+        # Maybe there is another, more informative check we could make here?
+        def check_feature_mean(mean_to_check, baseline_mean, allowed_difference):
+            return (abs(mean_to_check - baseline_mean) < allowed_difference).all()
+
+        assert check_feature_mean(global_test_mean, global_train_mean, global_train_std)
+
+        for local_train_set in training_slices.values():
+            local_train_mean = subset_for_class(local_train_set, class_index).mean(axis=0)
+            assert check_feature_mean(local_train_mean, global_train_mean, global_train_std)
+
+
+def test_write_read_scaling_dataset(tmp_path: pathlib.Path):
+    # use mostly default parameters from utils.parse_arguments()
+    n_classes = 10
+    n_features = 100
+    frac_informative = 0.1
+    frac_redundant = 0.1
+    make_classification_kwargs = {
+        "n_clusters_per_class": 1,
+        "n_informative": int(frac_informative * n_features),
+        "n_redundant": int(frac_redundant * n_features),
+        "flip_y": 0.01,
+    }
+    n_ranks = 10
+
+    # create scaling dataset for 10 ranks
+    global_train_set, training_slices, global_test_set = generate_scaling_dataset(
+        n_samples=1250,
+        n_features=n_features,
+        n_classes=n_classes,
+        n_ranks=n_ranks,
+        random_state=0,
+        test_size=0.2,
+        make_classification_kwargs=make_classification_kwargs,
+        stratified_train_test=True,
+    )
+
+    hdf5_path = tmp_path / "dataset.h5"
+    global_attrs = {"test": "test_attr"}
+
+    # write scaling dataset to HDF5
+    write_scaling_dataset_to_hdf5(global_train_set, training_slices, global_test_set, global_attrs, hdf5_path)
+
+    # check overriding: raises exception if override is false but overrides if true
+    pytest.raises(
+        FileExistsError,
+        lambda: write_scaling_dataset_to_hdf5(
+            global_train_set, training_slices, global_test_set, global_attrs, hdf5_path, override=False
+        ),
+    )
+    write_scaling_dataset_to_hdf5(
+        global_train_set, training_slices, global_test_set, global_attrs, hdf5_path, override=True
+    )
+
+    expected_root_attrs = {
+        **global_attrs,
+        "n_classes": n_classes,
+        "n_ranks": n_ranks,
+        "n_samples_global_train": global_train_set.n_samples,
+    }
+
+    # read entire scaling dataset from HDF5
+    read_local_train_sets, read_global_test_set, read_root_attrs = read_scaling_dataset_from_hdf5(hdf5_path)
+    assert read_global_test_set == global_test_set
+    assert read_local_train_sets == training_slices
+    assert read_root_attrs == expected_root_attrs
+
+    # read specific local training set from HDF5
+    for rank in range(n_ranks):
+        read_local_train_set, read_global_test_set, read_root_attrs = read_scaling_dataset_from_hdf5(
+            hdf5_path, rank=rank
+        )
+        assert read_global_test_set == global_test_set
+        assert read_local_train_set == training_slices[rank]
+        assert read_root_attrs == expected_root_attrs
