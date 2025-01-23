a6ab44b1bb20ebd2193a7c5728d02c666178bc98
Katharina Fluegel <katharina.fluegel@kit.edu>
2025-01-23 09:14:35 +0100

    Convert to numpy array when reading dataset from HDF5

--- specialcouscous/scaling_dataset.py
+++ specialcouscous/scaling_dataset.py
@@ -249,8 +249,8 @@ def read_scaling_dataset_from_hdf5(
 
     def dataset_from_group(group: h5py.Group) -> SyntheticDataset:
         return SyntheticDataset(
-            group["x"],
-            group["y"],
+            group["x"][...],
+            group["y"][...],
             n_samples=int(group.attrs["n_samples"]),
             n_classes=int(n_classes),
         )
