96944e7bd415706716bdc093e00e34b832be0ec6
Katharina Fluegel <katharina.fluegel@kit.edu>
2025-02-04 10:21:52 +0100

    Add script to check pre-generated datasets

---
+++ scripts/examples/check_scaling_dataset.py
@@ -0,0 +1,51 @@
+import argparse
+
+from specialcouscous.scaling_dataset import read_scaling_dataset_from_hdf5
+
+
+def load_and_check_dataset(dataset: str, n_ranks: int = 64) -> None:
+    """
+    Load and check a scaling dataset by loading the HDF5 file rank by rank and printing the dataset sizes.
+
+    Parameters
+    ----------
+    dataset : str
+        Name of the baseline single-node dataset (n6m4 or n7m3).
+    n_ranks : int
+        The maximum number of ranks, the check will load slices range(n_ranks) for the HDF5. Default is 64.
+    """
+    dataset_dirnames = {
+        "n6m4": "n_samples_48250000__n_features_10000__n_classes_10",
+        "n7m3": "n_samples_482500000__n_features_1000__n_classes_10",
+    }
+
+    dirname = dataset_dirnames[dataset]
+    hdf5_path = f"/hkfs/work/workspace/scratch/ku4408-SpecialCouscous/datasets/{dirname}/64_ranks__seed_0.h5"
+    print(f"{hdf5_path=}\n")
+
+    for rank in range(n_ranks):
+        local_train_set, global_test_set, root_attrs = read_scaling_dataset_from_hdf5(
+            hdf5_path, rank=rank
+        )
+
+        if rank == 0:
+            print(f"Root attrs:\n{root_attrs}\n")
+            print(
+                f"Global test set: {global_test_set.n_samples} samples "
+                f"with {global_test_set.x.shape[1]} features "
+                f"and {global_test_set.n_classes} classes.\n"
+            )
+
+        print(f"Rank {rank:2d}: Local train set: {global_test_set.n_samples} samples.")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        choices=["n6m4", "n7m3"],
+        help="Select the scaling dataset to check based on the name of it's single-node baseline",
+    )
+    args = parser.parse_args()
+    load_and_check_dataset(args.dataset)
