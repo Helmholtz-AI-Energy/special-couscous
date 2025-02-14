4e90b7382ec92a316764e2275de557b90078afc4
Katharina Fluegel <katharina.fluegel@kit.edu>
2025-02-14 19:59:19 +0100

    Add logging to breaking IID train class frequency analysis

--- scripts/examples/analyze_non_iid_data.py
+++ scripts/examples/analyze_non_iid_data.py
@@ -1,4 +1,6 @@
 import itertools
+import logging
+import math
 import pathlib
 import sys
 
@@ -9,6 +11,10 @@ from specialcouscous.synthetic_classification_data import (
     SyntheticDataset,
     generate_and_distribute_synthetic_dataset,
 )
+from specialcouscous.utils import set_logger_config
+
+
+log = logging.getLogger("specialcouscous")
 
 
 def compute_and_store_class_frequencies(
@@ -73,8 +79,12 @@ def store_class_frequencies_train(
     n_classes : int
         The number of classes, default 10.
     """
+    dataset = f'n{math.log10(n_samples)}_m{math.log10(n_features)}'
     dir_name = f"{random_state_data}_{str(mu_data).replace('.', '')}_{str(mu_partition).replace('.', '')}"
-    (base_path / dir_name).mkdir(exist_ok=True, parents=True)
+    out_dir = base_path / dataset / dir_name
+    out_dir.mkdir(exist_ok=True, parents=True)
+    log.info(f'Analyzing {dataset=}, {random_state_data=}, {mu_data=}, {mu_partition=}.')
+
     random_state_data = check_random_state(random_state_data)
     make_classification_kwargs = {
         "n_clusters_per_class": 1,
@@ -85,6 +95,7 @@ def store_class_frequencies_train(
     global_train_class_frequencies = np.zeros(n_classes)
 
     for rank in range(n_nodes):
+        log.debug(f'Generating dataset for {rank=}.')
         (_, local_train, local_test) = generate_and_distribute_synthetic_dataset(
             globally_balanced=False,
             locally_balanced=False,
@@ -100,26 +111,30 @@ def store_class_frequencies_train(
             peak=0,
             make_classification_kwargs=make_classification_kwargs,
         )
+        log.debug(f'Done generating dataset.')
 
-        path = base_path / dir_name / f"class_frequencies_train_rank_{rank}.csv"
+        path = out_dir / f"class_frequencies_train_rank_{rank}.csv"
         global_train_class_frequencies += compute_and_store_class_frequencies(
             path, local_train
         )
+        log.info(f'Local class frequencies written to {path}.')
 
-    path = base_path / dir_name / "class_frequencies_train_global.csv"
+    path = out_dir / "class_frequencies_train_global.csv"
     np.savetxt(path, global_train_class_frequencies, delimiter=",")
+    log.info(f'Global class frequencies written to {path}.')
 
 
 if __name__ == "__main__":
+    set_logger_config(level=logging.DEBUG)
+
     base_path = pathlib.Path(sys.argv[1])
-    print(f"Writing class frequencies to {base_path}")
+    log.info(f"Writing class frequencies to {base_path}.")
     mus: list[str | float] = ["inf", 2., 0.5]
     datasets = [(1e6, 1e4), (1e7, 1e3)]
 
     for (n_samples, n_features), mu_data, mu_partition in itertools.product(
         datasets, mus, mus
     ):
-        print(n_samples, n_features, mu_data, mu_partition)
         store_class_frequencies_train(
             base_path, int(n_samples), int(n_features), mu_data, mu_partition
         )
