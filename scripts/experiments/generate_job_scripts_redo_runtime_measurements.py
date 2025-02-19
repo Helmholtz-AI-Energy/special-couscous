1717a1f0ad629bfc09683050dc308b13577c19f9
Katharina Fluegel <katharina.fluegel@kit.edu>
2025-02-19 11:22:04 +0100

    Update time limit estimation
    - ceil instead of floor to int minutes
    - update baseline time for inference flavors

--- scripts/experiments/generate_job_scripts_redo_runtime_measurements.py
+++ scripts/experiments/generate_job_scripts_redo_runtime_measurements.py
@@ -1,4 +1,5 @@
 import itertools
+import math
 import pathlib
 from typing import Any
 
@@ -17,8 +18,8 @@ SERIAL_BASELINE_TIMES = {  # in minutes
     (7, 3, 448): 227,
     (6, 4, 800): 138,  # estimate from 1600 trees (=)
     (7, 3, 224): 227,  # estimate from 448 trees (=)
-    (5, 3, 76): 3,  # estimate from 800 trees (=)
-    (6, 2, 76): 4,  # estimate from 224 trees (=)
+    (5, 3, 76): 1,
+    (6, 2, 76): 2,
 }
 OVERESTIMATION_FACTOR = 2  # overestimate time limit by how much from expected time
 
@@ -173,7 +174,9 @@ def generate_strong_scaling_job_scripts(
         datasets, data_seeds, model_seeds, comm_sizes
     ):
         # assumes 25% un-parallelizable workload (data generation, communication, saving results,...)
-        expected_time = int(SERIAL_BASELINE_TIMES[dataset] * (0.25 + 0.75 / comm_size))
+        expected_time = int(
+            math.ceil(SERIAL_BASELINE_TIMES[dataset] * (0.25 + 0.75 / comm_size))
+        )
         log_n_samples, log_n_features, n_trees = dataset
         label = f"strong_scaling/n{log_n_samples}_m{log_n_features}/n_nodes_{comm_size}/{data_seed}_{model_seed}"
         run_specific_configs = {
@@ -223,7 +226,9 @@ def generate_weak_scaling_job_scripts(
         datasets, data_seeds, model_seeds, comm_sizes
     ):
         # assumes 1% overhead per node
-        expected_time = int(SERIAL_BASELINE_TIMES[dataset] * (1 + 0.01 * comm_size))
+        expected_time = int(
+            math.ceil(SERIAL_BASELINE_TIMES[dataset] * (1 + 0.01 * comm_size))
+        )
         log_n_samples, log_n_features, n_trees_local = dataset
         label = f"weak_scaling/n{log_n_samples}_m{log_n_features}/n_nodes_{comm_size}/{data_seed}_{model_seed}"
         run_specific_configs = {
@@ -274,7 +279,9 @@ def generate_chunking_job_scripts(
         datasets, data_seeds, model_seeds, comm_sizes
     ):
         # same as strong scaling (ignores additional speedup from chunking)
-        expected_time = int(SERIAL_BASELINE_TIMES[dataset] * (0.25 + 0.75 / comm_size))
+        expected_time = int(
+            math.ceil(SERIAL_BASELINE_TIMES[dataset] * (0.25 + 0.75 / comm_size))
+        )
         log_n_samples, log_n_features, n_trees = dataset
         label = f"chunking/n{log_n_samples}_m{log_n_features}/n_nodes_{comm_size}/{data_seed}_{model_seed}"
         run_specific_configs = {
@@ -329,16 +336,13 @@ def generate_inference_flavor_job_scripts(
         for shared_global_model in [True, False]:
             if shared_global_model:
                 # assumes 50% overhead per node (for model gathering & evaluation)
-                expected_time = int(
-                    SERIAL_BASELINE_TIMES[dataset] * (1 + 0.5 * comm_size)
-                )
+                expected_time = SERIAL_BASELINE_TIMES[dataset] * (1 + 0.5 * comm_size)
                 shared_model = "shared_model"
             else:
                 # same as weak scaling, assumes 1% overhead per node
-                expected_time = int(
-                    SERIAL_BASELINE_TIMES[dataset] * (1 + 0.01 * comm_size)
-                )
+                expected_time = SERIAL_BASELINE_TIMES[dataset] * (1 + 0.01 * comm_size)
                 shared_model = "no_shared_model"
+            expected_time = int(math.ceil(expected_time))
             label = (
                 f"inference_flavor/n{log_n_samples}_m{log_n_features}/{shared_model}/"
                 f"n_nodes_{comm_size}/{data_seed}_{model_seed}"
@@ -360,6 +364,62 @@ def generate_inference_flavor_job_scripts(
             }
             config = {**global_config, **run_specific_configs}
             generate_job_script(base_job_script_path / f"{label}.sh", config)
+
+
+def generate_small_scale_model_job_scripts(
+    global_config: dict[str, Any],
+    data_seeds: list[int],
+    model_seeds: list[int],
+    result_base_dir: pathlib.Path,
+    base_job_script_path: pathlib.Path,
+) -> None:
+    """
+    Generate all serial job scripts.
+
+    Parameters
+    ----------
+    global_config : dict[str, Any]
+        The global job script configuration. Should contain the following keys: project, script_dir, venv, n_classes,
+        mem, additional_args, partition. Keys mem, partition, and all other keys may be overwritten.
+    data_seeds : list[int]
+        The list of data seeds.
+    model_seeds : list[int]
+        The list of model seeds.
+    result_base_dir : pathlib.Path
+        The base directory to write the job results to (subdirectories for experiment and run config will be created).
+    base_job_script_path : pathlib.Path
+        The base directory to write the job scripts to (subdirectories for experiment and run config will be created).
+    """
+    datasets = [*DATASETS["strong"], *DATASETS["weak"], *DATASETS["inference"]]
+    comm_size = 1
+    for dataset, data_seed, model_seed in itertools.product(
+        datasets, data_seeds, model_seeds
+    ):
+        expected_time = SERIAL_BASELINE_TIMES[dataset]
+        log_n_samples, log_n_features, n_trees = dataset
+        label = f"serial_baseline/n{log_n_samples}_m{log_n_features}_t{n_trees}/{data_seed}_{model_seed}"
+        run_specific_configs = {
+            "job_name": label,
+            "n_samples": 10**log_n_samples,
+            "n_features": 10**log_n_features,
+            "n_trees": n_trees,
+            "random_state_data": data_seed,
+            "random_state_model": model_seed,
+            "time": min(expected_time * OVERESTIMATION_FACTOR, MAX_TIME),
+            "n_nodes": comm_size,
+            "result_dir": result_base_dir / label,
+            "script": "rf_serial_synthetic.py",
+        }
+        # for strong scaling baselines -> use high memory node
+        if n_trees in [1600, 448]:
+            high_mem_config = {
+                "mem": "501600mb",
+                "time": min(run_specific_configs["time"], MAX_TIME_HIGH_MEM),
+                "partition": "large",
+            }
+            run_specific_configs = {**run_specific_configs, **high_mem_config}
+        config = {**global_config, **run_specific_configs}
+        generate_job_script(base_job_script_path / f"{label}.sh", config)
 
 
 if __name__ == "__main__":
