fc89131f9d33741493fe7e18cc6d10c913062bd7
Katharina Fluegel <katharina.fluegel@kit.edu>
2025-06-17 15:43:13 +0200

    Add test case for parallel vs serial prediction

--- tests/test_rf_parallel_evaluation.py
+++ tests/test_rf_parallel_evaluation.py
@@ -1,3 +1,4 @@
+import copy
 import logging
 import pathlib
 
@@ -154,3 +155,79 @@ def test_accuracy_score_vs_evaluate(
         )
 
     assert score_accuracy == distributed_random_forest.acc_global
+
+
+@pytest.mark.mpi
+@pytest.mark.parametrize("shared_global_model", [True, False])
+def test_serial_vs_parallel_prediction(
+    shared_global_model: bool,
+    synthetic_dataset: tuple[SyntheticDataset, SyntheticDataset],
+    n_trees: int = 100,
+    model_seed: int = 0,
+) -> None:
+    """
+    Test the prediction of parallel random forests by comparing them to the serial prediction.
+
+    Parameters
+    ----------
+    shared_global_model : bool
+        Whether to use a shared global model or distributed voting for inference.
+    synthetic_dataset : tuple[SyntheticDataset, SyntheticDataset]
+        The train and test dataset.
+    n_trees : int
+        The number of trees in the model to test.
+    model_seed : int
+        The random seed used for model creation.
+    """
+    comm = MPI.COMM_WORLD
+    train_data, test_data = synthetic_dataset
+
+    # Train distributed RF
+    if comm.rank == 0:
+        log.info("Create and train random forest")
+    parallel_rf = DistributedRandomForest(
+        n_trees_global=n_trees,
+        comm=comm,
+        random_state=model_seed,
+        shared_global_model=shared_global_model,
+    )
+    parallel_rf.train(train_data.x, train_data.y)
+    if shared_global_model:
+        parallel_rf.build_shared_global_model()
+
+    # Create corresponding node-local sklearn RF consisting of all trees
+    if comm.rank == 0:
+        log.info("Create serial random forest for comparison")
+    serial_rf = copy.deepcopy(parallel_rf.clf)
+    if shared_global_model:
+        serial_rf.estimators_ = parallel_rf.trees
+    else:  # without global model -> need to gather trees from local subforests first
+        serial_rf.estimators_ = parallel_rf._allgather_subforests_tree_by_tree()
+
+    # Compare predictions and accuracy score between serial and distributed RF
+    if comm.rank == 0:
+        log.info("Evaluate serial forest on test set")
+    with timing.MPITimer(comm, name="serial_predict") as t_serial_predict:
+        serial_prediction = serial_rf.predict(test_data.x)
+    with timing.MPITimer(comm, name="serial_score") as t_serial_score:
+        serial_accuracy = serial_rf.score(test_data.x, test_data.y)
+
+    if comm.rank == 0:
+        log.info("Evaluate distributed forest on test set")
+    with timing.MPITimer(comm, name="parallel_predict") as t_parallel_predict:
+        parallel_prediction = parallel_rf.predict(test_data.x, test_data.n_classes)
+    with timing.MPITimer(comm, name="parallel_score") as t_parallel_score:
+        parallel_accuracy = parallel_rf.score(
+            test_data.x, test_data.y, test_data.n_classes
+        )
+    timers = [t_serial_predict, t_serial_score, t_parallel_predict, t_parallel_score]
+
+    if comm.rank == 0:
+        log.info(f"Test accuracy: {serial_accuracy=}, {parallel_accuracy=}")
+        formatted_runtimes = ", ".join(
+            f"{timer.name}={timer.elapsed_time_average:.2f}s" for timer in timers
+        )
+        log.info(f"Runtimes: {formatted_runtimes}")
+
+    assert all(serial_prediction == parallel_prediction)
+    assert serial_accuracy == parallel_accuracy
